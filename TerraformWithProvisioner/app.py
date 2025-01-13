bd5ae42635a68be521abed8d9d8cebb7e1c562b3
Purushotham Kandheri <87255292+purushotham5@users.noreply.github.com>
2025-01-13 19:32:26 +0530

    Update app.py

--- TerraformWithProvisioner/app.py
+++ TerraformWithProvisioner/app.py
@@ -1,3 +1,10 @@
+# to install python packages or flask on the ec2 use this bellow following commands 
+# # First Login to the ec2 instances then follow 
+# sudo apt update -y
+# sudo apt-get install -y python3-flask
+# sudo python3 app.py { here it will run the port 80, then go the ec2 instaces open the public ip address in new tab we will get the python code output }
+
+
 from flask import Flask
 
 app = Flask(__name__)
